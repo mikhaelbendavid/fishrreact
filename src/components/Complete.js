d697ea16b5ead6424283081bf0848a11b6e9789d
Patrick Richardson <setfloat@gmail.com>
2018-02-11 14:55:38 -0800

    add form submittion

---
+++ src/components/Complete.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export default () => {
+  return (<div>
+      <h3 >Complete</h3>
+      <h4 >Port:</h4><h5 > Kisumu</h5>
+      <br />
+      <h4 >Fish:</h4><h5 > Nile Perch</h5>
+      <br />
+      <h4 >Buyer:</h4><h5 > Michael Litchev</h5>
+      <br />
+      <h4 >Seller:</h4><h5 > Steve Zisu</h5>
+      <br />
+      <h4 >Kilos:</h4><h5 > 10</h5>
+      <br />
+      <h4>Price:</h4><h5> $20</h5>
+      <br />
+      <h4>Comments:</h4><h5> Smelly fish!</h5>
+    </div>)
+}
